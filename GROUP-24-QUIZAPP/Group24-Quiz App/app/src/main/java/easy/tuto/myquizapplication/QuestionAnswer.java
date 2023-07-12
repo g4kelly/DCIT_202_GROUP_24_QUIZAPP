1924df0273201d8ab13ffcb563067cdfe209c05b
1realme <g4.kelly@icloud.com>
2023-07-12 14:17:05 -0600

    Our group Project

---
+++ GROUP-24-QUIZAPP/Group24-Quiz App/app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -0,0 +1,26 @@
+package easy.tuto.myquizapplication;
+
+public class QuestionAnswer {
+
+    public static String question[] ={
+            "Who was the first Prime Minister of Ghana",
+            "Which one is not the programming language?",
+            "Which of the following is not a computer Brand?",
+            "Which company owns the Apple?"
+    };
+
+    public static String choices[][] = {
+            {"K. Nkrumah","K.A Busia","J. J. Rawlings","J. Mahama"},
+            {"Java","Kotlin","Notepad","Python"},
+            {"Apple","Windows","JacknJill","Dell"},
+            {"Google","Apple","Nokia","Samsung"}
+    };
+
+    public static String correctAnswers[] = {
+            "K.Nkrumah",
+            "Notepad",
+            "JacknJill",
+            "Apple"
+    };
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # GROUP_24_QUIZAPP
 Group project for dcit 202
+# Quizapp
+ A simple quizapp
+This is the project for Group 24 (Dcit 202);
+
+Members include:
+1. Kevin Jones Lamptey (10983384) - Administrator.
+2. Osei Asante Yort (10984534) - In charge of the teams social media handles
+3. Kludje Shadrack(10961673) - Wrote the first screen of the project and organised the xml liables.
+4. Jospeh Mensah (10990046) - Manage the github accounts and organised the quiz questions.
+5.Imbeah Fiifi Caleb ( 10984058) - Wrote the second screen for the quiz app.
+
+
+The app gives a simple quiz for users to answer. No account is needed to access the content, just a simple app that gives a list of questions, records the user scores
+and shows all incorrect answers. 
